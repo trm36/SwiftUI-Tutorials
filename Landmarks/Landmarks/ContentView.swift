7f323475c6ead3f2aae67c25dea6298e0ae40c6a
Taylor Mott <mott.taylor@gmail.com>
2022-06-13 11:10:32 -0600

    1-2-6 Sets Up Navigation Between List and Detail

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,41 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300.0)
-            
-            CircleImage()
-                .offset(y: -130.0)
-                .padding(.bottom, -130.0)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.pink)
-                
-                HStack {
-                    Text("Joshua Tree National Park")
-                    
-                    Spacer()
-                    
-                    Text("California, USA")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
+        NavigationView {
+            LandmarkList()
         }
     }
 }
---
+++ Landmarks/Landmarks/Custom Views/LandmarkDetail.swift
@@ -0,0 +1,55 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Taylor on 13 June 2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300.0)
+            
+            CircleImage()
+                .offset(y: -130.0)
+                .padding(.bottom, -130.0)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.pink)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    
+                    Spacer()
+                    
+                    Text("California, USA")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
