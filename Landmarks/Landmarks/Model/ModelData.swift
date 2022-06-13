5c745fad138696e9cea092da8524d3344635da19
Taylor Mott <mott.taylor@gmail.com>
2022-06-13 01:08:03 -0600

    Creates a Landmark Model

---
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -0,0 +1,32 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Taylor on 13 June 2022.
+//
+
+import Foundation
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("🚨 Could not find \(filename) in main bundle.")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("🚨 Could not load \(filename) from main bundle:\n\(error)")
+    }
+    
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("🚨 Could not parse \(filename) as \(T.self):\n\(error)"
+    }
+    
+}
